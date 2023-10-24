d1c05970c0032dd21fb6408c14591cc22adc6711
Paco <paco@mastermind.ac>
2023-10-24 11:01:52 +0200

    Layout

--- escooter/app/page.js
+++ escooter/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Hola Paco</h1>
+      <h1 className="text-success">Hola Paco</h1>
     </main>
   )
 }
