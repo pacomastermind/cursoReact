81a8904b26d4b84d3ea8aaeacaf364b7d2515779
Paco <paco@mastermind.ac>
2023-11-03 13:06:07 +0100

    Scooter Top

--- escooter/app/scooter/page.js
+++ escooter/app/scooter/page.js
@@ -1,6 +1,18 @@
 import ScooterCard from "../component/ScooterCard"
+import ScooterTop from "../component/ScooterTop"
 
-//Get data from json-server
+//Get data from json-server, Top Products
+async function getTopProducts() {
+    const res = await fetch('http://localhost:4000/products?destacado=1', { next: { revalidate: 86400 } })
+   
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
+
+//Get data from json-server, Products
 async function getProducts() {
     const res = await fetch('http://localhost:4000/products')
    
@@ -13,23 +25,25 @@ async function getProducts() {
 
 export default async function Scooter() {
     const products = await getProducts()
+    const topProducts = await getTopProducts()
     console.log(products)
+    console.log(topProducts)
 
     return (
     <>
     {/* CONTENIDO */}
     <div className="contPrincipal container mb-5">
-        <h2>Scooters ({products.length}) </h2>
+        <h2>Scooters ({products.lenght}) </h2>
         <div className="grid">
         <div className="row mt-3">
             <div className="col-8">
             {/*DESTACADOS TARJETAS*/}
             <div className="d-flex p-2 flex-wrap justify-content-between">
             {
-                products.map(product=>(
-                    <ScooterCard key={product.id} img={product.img} modelo={product.modelo} txt={product.txt}/>
+                    products.map(product=>(
+                        <ScooterCard key={product.id} img={product.img} modelo={product.modelo} txt={product.txt}/>
+                        )
                     )
-                )
             }
             </div>
             </div>
@@ -40,18 +54,12 @@ export default async function Scooter() {
                 <h4>Top</h4>
                 </div>
                 <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                    Xiaomi Mi Electric Scooter Pro{" "}
-                    <span className="badge text-bg-dark float-end">546 €</span>
-                </li>
-                <li className="list-group-item">
-                    Bongo Serie S{" "}
-                    <span className="badge text-bg-dark float-end">375 €</span>
-                </li>
-                <li className="list-group-item">
-                    Xiaomi Mi Electric Scooter{" "}
-                    <span className="badge text-bg-dark float-end">250 €</span>
-                </li>
+                {
+                    topProducts.map(topProduct=>(
+                        <ScooterTop key={topProduct.id} modelo={topProduct.modelo} precio={topProduct.precio}/>
+                        )
+                    )
+                }
                 </ul>
             </div>
             </div>
